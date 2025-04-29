9914d8a5b90383eaeaf0e4138d4919c4c2908bca
Re Solver <sled-79-slender@icloud.com>
2025-04-28 17:52:37 -0700

    Initial Commit

---
+++ hopscotchTests/hopscotchTests.swift
@@ -0,0 +1,17 @@
+//
+//  hopscotchTests.swift
+//  hopscotchTests
+//
+//  Created by Abhimanyu Yadav on 4/28/25.
+//
+
+import Testing
+@testable import hopscotch
+
+struct hopscotchTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
